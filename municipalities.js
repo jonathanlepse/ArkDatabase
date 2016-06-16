2c2e23e80ce5460fb56c004e30845ad406b31633
Emil Choski <emilchoski@gmail.com>
2016-06-16 10:06:36 -0400

    initial commit

---
+++ municipalities.js
@@ -0,0 +1,17 @@
+/**
+ * Created by emilchoski on 6/16/16.
+ */
+var municipalities = {
+    list: [
+        {
+            town_name_full: "City of Glen Cove",
+            town_name_short: "Glen Cove"
+        }
+
+
+
+
+    ]
+
+
+};
