913d188f14e8b910bccd68eecd818e975be57fc2
Emil Choski <emilchoski@gmail.com>
2016-06-18 19:06:00 -0400

    basic screen scraper code

--- node/arkdata/app.js
+++ node/arkdata/app.js
@@ -1,3 +1,86 @@
+var express    = require('express');
+var request    = require('request');
+var cheerio    = require('cheerio');
+var nodemailer = require('nodemailer');
+var CronJob    = require('cron').CronJob;
+var fs         = require('fs');
+var app        = express();
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+  service: 'YOUR_SERVICE',
+  auth: {
+    user: 'YOUR_EMAIL_ACCOUNT',
+    pass: 'YOUR_PASSWORD'
+  }
+});
+
+app.get('/scrape', function(req, res){
+
+  var $, scraped_data, url;
+  url = "http://ecode360.com/10414303";
+
+  request(url, function(error, response, html) {
+
+    if(!error) {
+
+      $ = cheerio.load(html);
+
+
+
+
+      $('#toc .titleTitle').each(function(i, elem) {
+        console.log($(this).text());
+      });
+
+
+
+      console.log(scraped_data);
+
+      return;
+
+      // Send an email
+      var mailOptions = {
+        from: 'YOUR_SENDER',
+        to: 'YOUR_RECIPIENT',
+        subject: 'YOUR_SUBJECT',
+        text: 'YOUR_MESSAGE' + scraped_data
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, function(error, info){
+        if(error) {
+          return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+      });
+    }
+  });
+});
+
+/*
+
+// How frequently should the job run
+new CronJob('0 0 * * *', function(){
+  request.get('YOUR_HOST/scrape')
+}, null, true, "America/Los_Angeles");
+
+app.listen('8081')
+console.log('Listening on port 8081');
+
+*/
+
+exports = module.exports = app;
+
+
+
+
+
+
+
+
+
+return;
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
