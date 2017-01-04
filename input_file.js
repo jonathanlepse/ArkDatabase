bd45d0c653f57946593e13264c43a0a768a248bd
Emil Choski <emilchoski@gmail.com>
2017-01-04 07:11:39 -0500

    moved input to a file

---
+++ input_file.js
@@ -0,0 +1,140 @@
+var encoded_rules =
+
+
+        [
+            {
+                "clauseID": "@§_271-28@A.",
+                "rules": [
+                    {
+                        "rule": "MAX_HEIGHT",
+                        "args": [
+                            {
+                                "key": "max",
+                                "value": "31",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-28@B.",
+                "rules": [
+                    {
+                        "rule": "MAX_HEIGHT_FOR_ACCESSORY_BUILDING",
+                        "args": [
+                            {
+                                "key": "max",
+                                "value": "20",
+                                "units": "FEET"
+                            }
+                        ]
+                    },
+                    {
+                        "rule": "MAX_HEIGHT_FOR_ACCESSORY_BUILDING",
+                        "args": [
+                            {
+                                "key": "max",
+                                "value": "20",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-30",
+                "rules": [
+                    {
+                        "rule": "MAX_LOT_COVERAGE",
+                        "args": [
+                            {
+                                "key": "max",
+                                "value": "25",
+                                "units": "PERCENTAGE"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-31",
+                "rules": [
+                    {
+                        "rule": "MIN_FLOOR_AREA",
+                        "args": [
+                            {
+                                "key": "min",
+                                "value": "1500",
+                                "units": "FEET_SQUARE"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-32@A.",
+                "rules": [
+                    {
+                        "rule": "MIN_SETBACK_FOR_FRONT_YARD",
+                        "args": [
+                            {
+                                "key": "min",
+                                "value": "35",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-32@B.",
+                "rules": [
+                    {
+                        "rule": "MIN_SETBACK_AGGREGATE_SIDE_YARD",
+                        "args": [
+                            {
+                                "key": "min",
+                                "value": "40",
+                                "units": "FEET"
+                            },
+                            {
+                                "key": "individual_min",
+                                "value": "15",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-32@C.",
+                "rules": [
+                    {
+                        "rule": "MIN_SETBACK_FOR_BACK_YARD",
+                        "args": [
+                            {
+                                "key": "min",
+                                "value": "30",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "clauseID": "@§_271-32@D.",
+                "rules": [
+                    {
+                        "rule": "MIN_SETBACK_FOR_YARD_FROM_STREETLINE_IF_CORNER_LOT",
+                        "args": [
+                            {
+                                "key": "min",
+                                "value": "30",
+                                "units": "FEET"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
